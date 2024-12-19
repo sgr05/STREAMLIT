837fef92aa798f074f5cff1c5f60594b8b4db100
sgr05 <sagarsanil005@gmail.com>
2024-12-19 11:07:08 +0530

    second

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from os import path
 import numpy as np
-data=pd.read_csv(path.join("data","/Users/sagar/Downloads/auto-mpg.csv"))
+data=pd.read_csv(path.join("auto-mpg.csv"))
 st.title("Welcome to my streamlit website")
 st.write("Hellow")
 df=pd.DataFrame({"first col":[1,2,3,4],"second col":[10,20,30,40] })
@@ -11,7 +11,7 @@ st.write(df["first col"])
 st.write(data)
 st.write(data["mpg"])
 #map_data=pd.DataFrame(
-    #np.array([["8.55709","76.8819745"]]),columns=["lat","lon"])
+ #   np.array([["8.55709","76.8819745"]]),columns=["lat","lon"])
 #st.map(map_data)
 x=st.slider("x",min_value=10,max_value=200)
 st.title("{} squared is {}".format(x,x*x))
